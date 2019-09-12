9f70db453b60c462d9d050fdaa6823818218b305
vtorres96 <victorcdc96@gmail.com>
2019-09-12 11:48:00 -0300

    adding home page

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import ListarTarefas from '../components/ListarTarefas'
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div>
+                <ListarTarefas />
+            </div>
+        )
+    }
+}
+
+export default Home
